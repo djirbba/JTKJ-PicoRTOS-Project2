14f7eea72678fd24ad2743cf8a90f5f6ce52fc19
SatVak <satuvakkuri04@gmail.com>
2025-11-06 11:38:59 +0200

    Update main.c

--- examples/hat_imu_ex/src/main.c
+++ examples/hat_imu_ex/src/main.c
@@ -34,7 +34,7 @@ void imu_task(void *pvParameters) {
     {
         if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &t) == 0) {
             
-            printf("Accel: X=%f, Y=%f, Z=%f | Gyro: X=%f, Y=%f, Z=%f| Temp: %2.2f°C\n", ax, ay, az, gx, gy, gz, t);
+            printf("Accel: X=%f, Y=%f, Z=%f\n", ax, ay, az);
 
         } else {
             printf("Failed to read imu data\n");
