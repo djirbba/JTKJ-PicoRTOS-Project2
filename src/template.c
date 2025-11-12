7c43414d663b6d7dd498ea8f58b7d22e0745d916
djirbba <iiris.k.pelkonen@gmail.com>
2025-11-12 14:38:57 +0200

    Tilakoneen aloitus ja mittauksien alustusta

--- src/template.c
+++ src/template.c
@@ -12,17 +12,32 @@
 
 // Default stack size for the tasks. It can be reduced to 1024 if task is not using lot of memory.
 #define DEFAULT_STACK_SIZE 2048 
+#define LIIKKEEN_RAJA 1? // valitaan arvo, joka pitää ylittää että voidaan määrittää liikkuuko sensori vai ei 
 
 //Add here necessary states
-enum state { IDLE=1 };
+enum state { IDLE=1, STATE_MOVING};
 enum state programState = IDLE;
 
-static void example_task(void *arg){
-    (void)arg;
 
-    for(;;){
-        tight_loop_contents(); // Modify with application code here.
-        vTaskDelay(pdMS_TO_TICKS(2000));
+
+static void update_task(float ax, float ay, float az){  // Alustaa ensimmäiset arvot, että voidaan tulkita onko liikettä tapahtunut (on jokin mihin verrata)
+    float aikaisempi_ax = 0, aikaisempi_ay = 0, aikaisempi_az = 0;
+    static bool first_read = true; // True, jolloin seuraava funktio alkaa pyörimään
+
+    if (first_read){
+        aikaisempi_ax = ax;
+        aikaisempi_ay = ay;
+        aikaisempi_az = az;
+        first_read = false; // false, että ensimmäinen mittaus ja sen arvot kerätään vain kerran -> task tapahtuu vain kerran
+        return;
+    }
+
+     // lasketaan seuraavaksi arvojen erotus, josta voidaan päätellä onko laite liikkeellä vai pysähtyneenä
+    float erotus = fabs(aikaisempi_ax - ax) + fabs(aikaisempi_ay - ay) + fabs(aikaisempi_az - az);
+
+    //for(;;){
+       // tight_loop_contents(); // Modify with application code here.
+      //  vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
 
