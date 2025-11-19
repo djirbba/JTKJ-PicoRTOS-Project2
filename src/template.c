49e15424a1717e50da14e106a60534e1f62a5b67
SatVak <satuvakkuri04@gmail.com>
2025-11-19 13:20:15 +0200

    Update template.c

--- src/template.c
+++ src/template.c
@@ -13,7 +13,6 @@
 // Default stack size for the tasks. It can be reduced to 1024 if task is not using lot of memory.
 #define DEFAULT_STACK_SIZE 2048 
 #define LIIKKEEN_RAJA 1? // valitaan arvo, joka pitää ylittää että voidaan määrittää liikkuuko sensori vai ei 
-
 //TILAN PÄIVITYS
 
 //Add here necessary states
@@ -50,8 +49,40 @@ static void update_task(float ax, float ay, float az){  // Alustaa ensimmäiset 
 //TILAN PÄIVITYS LOPPUU
 
 
+//IMU-ARVOJEN LUKEMINEN
 
 
+void imu_task(void *pvParameters) {
+    (void)pvParameters;
+    
+    float ax, ay, az, gx, gy, gz, t;
+    // Setting up the sensor. 
+    if (init_ICM42670() == 0) {
+        printf("ICM-42670P initialized successfully!\n");
+        if (ICM42670_start_with_default_values() != 0){
+            printf("ICM-42670P could not initialize accelerometer or gyroscope");
+        }
+        /*int _enablegyro = ICM42670_enable_accel_gyro_ln_mode();
+        printf ("Enable gyro: %d\n",_enablegyro);
+        int _gyro = ICM42670_startGyro(ICM42670_GYRO_ODR_DEFAULT, ICM42670_GYRO_FSR_DEFAULT);
+        printf ("Gyro return:  %d\n", _gyro);
+        int _accel = ICM42670_startAccel(ICM42670_ACCEL_ODR_DEFAULT, ICM42670_ACCEL_FSR_DEFAULT);
+        printf ("Accel return:  %d\n", _accel);*/
+    } else {
+        printf("Failed to initialize ICM-42670P.\n");
+    }
+    // Start collection data here. Infinite loop. 
+    while (1)
+    {
+        if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &t) == 0) {
+            update_task(ax, ay, az);
+        }
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+
+}
+
+//IMU-ARVOT LUETTU
 
 
 
